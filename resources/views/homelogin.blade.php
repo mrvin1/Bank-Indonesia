f15a4b7f1d2946d99a96e56750526067a8f9763b
mrvin1 <vincent.fanditama@gmail.com>
2022-03-01 16:56:15 +0700

    homepage view

---
+++ resources/views/homelogin.blade.php
@@ -0,0 +1,5 @@
+@include('navbar')
+
+<h2 style="padding: 4%">Selamat datang </h2>
+
+@include('layouts.footer')
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="text-center text-lg-start" style="margin-top: 1rem">
+    <div class="text-center p-3">
+        © Bank Indonesia <br>
+    </div>
+</footer>
