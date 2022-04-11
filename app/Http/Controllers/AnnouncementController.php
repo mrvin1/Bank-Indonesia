99507fc7d3d96449ee55efeee0040a84ff088acf
mrvin1 <vincent.fanditama@gmail.com>
2022-04-11 11:23:01 +0700

    Update AnnouncementController.php

--- app/Http/Controllers/AnnouncementController.php
+++ app/Http/Controllers/AnnouncementController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Agenda;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 class AnnouncementController extends Controller
 {
     public function viewAnnouncement(){
         $auth = Auth::user()->email;
-        $agenda=Agenda::where('email', $auth)->paginate(5);
+        $now = Carbon::now();
+        $agenda=Agenda::where('email', $auth)->where('end', '>=',Carbon::now())->paginate(5);
         return view('announcement',['agenda'=>$agenda]);
     }
 }
