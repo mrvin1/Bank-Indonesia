570c8d85170ff3830ab7646d1969980ac3ce7a31
mrvin1 <vincent.fanditama@gmail.com>
2022-04-21 10:14:13 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
 
-                <div class="card-body">
+                <div class="card-body" style="margin-bottom: 2%">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
